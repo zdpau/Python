4f9b640b32822fbd23b97e5454694e7810210118
Zhang Duo <zhangduo2015@yahoo.co.jp>
2018-10-16 16:48:39 +0900

    Update 杨辉三角.py

--- liao/zuoye/杨辉三角.py
+++ liao/zuoye/杨辉三角.py
@@ -5,10 +5,10 @@ def triangles():
         l = [1] + [l[k] + l[k+1] for k in range(len(l)-1)] + [1]
 
 n = 0
-results = []
+results = [] # 这行可以不要
 for t in triangles():
     print(t)
-    results.append(t)
+    results.append(t) # 这行可以不要
     n = n + 1
     if n == 10:
         break
