e95978c4e1967c1932e9e36e40a550d0aaa36359
Zhang Duo <zhangduo2015@yahoo.co.jp>
2018-10-24 17:18:22 +0900

    Update 用map()把名字变为首字母大写，其他小写.py

--- liao/zuoye/用map()把名字变为首字母大写，其他小写.py
+++ liao/zuoye/用map()把名字变为首字母大写，其他小写.py
@@ -1,6 +1,7 @@
 def normalize(n):
     # 基本想到了，但是对ＭＡＰ函数还是不了解，通过ｍａｐ函数其实就会将normalize函数依次作用到序列的每个元素。
     return n[0].upper() + n[1:].lower()
+    # 也可以直接names.title()
     
 L1 = ['adam', 'LISA', 'barT']
 L2 = list(map(normalize, L1))
