d9838ffb8195dcf6d87002a480273e680c553028
Zhang Duo <zhangduo2015@yahoo.co.jp>
2018-10-24 17:34:30 +0900

    Create 接受一个list并利用reduce()求积.py

---
+++ liao/zuoye/接受一个list并利用reduce()求积.py
@@ -0,0 +1,15 @@
+# Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积
+from functools import reduce
+
+def prod(L):
+    def pro(x,y):
+        return x*y
+
+    return reduce(pro,L)
+   # return reduce(lambda x,y:x*y, L)
+
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
